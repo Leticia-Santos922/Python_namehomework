47eb4eb084cd61d42b425c3110a21558e7861f1b
leticia <leticia.nuness.santos@gmail.com>
2024-01-22 17:32:40 +0000

    initial commit on python homework

---
+++ say_my_name.py
@@ -0,0 +1,12 @@
+# variable name is assigned to string 'leticia'
+name = 'Leticia'
+# variable surname is assigned to string 'Santos'
+surname = 'Santos'
+
+# print function the two variables plus added a string sentence
+print('Hi my name is', name, surname)
+
+# what are variables?
+# variables are referenced to objects, they are containers for storing data values
+# is print a function?
+# yes print is a function - a function is a block of code which only runs when it is called
